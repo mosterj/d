98317d48d6728b78bd7f30f8aacae40c718edbfa
Ali Ahmed <ali365dash@gmail.com>
2017-08-11 14:35:09 +0100

    added blacklist and added messageCreate event (albeit empty atm)

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var fs = require('fs'),
 var obj;
 var token;
+var blacklist;
+const ROBLOX = require('roblox-js');
+const Eris = require('eris');
 fs.readFile('data/settings.json', 'utf8', function (err, data) {
     if (err) throw err;
     obj = JSON.parse(data);
     token = obj[0].token
+    blacklist = obj[0].blacklist
 });
 var bot = new Eris(token)
+
+bot.on('ready', () => {
+    console.log('Logged in')
+});
+
+bot.on('messageCreate', (msg) => {
+
+});
